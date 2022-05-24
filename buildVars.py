1cdfcd330ac4860b016fd3f8fff386373dd4a684
David Parduhn <xkill85@gmx.net>
2022-05-24 11:00:16 +0200

    - aktualisiert auf vda 2022.1

--- buildVars.py
+++ buildVars.py
@@ -19,14 +19,14 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Diese Erweiterung verbessert die Zugaenglichkeit von nordvpn durch Beschriften vieler Elemente."""),
 	# version
-	"addon_version" : "0.5",
+	"addon_version" : "0.6",
 	# Author(s)
 	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
-	"lastTestedNVDAVersion" : "2021.1",
+	"lastTestedNVDAVersion" : "2022.1",
 }
 
 
